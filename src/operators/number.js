6441d90e3923534c98a137cbadbccbd8abbacafd
diegodisant <developerdiego0@gmail.com>
2022-08-25 21:02:10 -0500

    learn: string and number operators

---
+++ src/operators/number.js
@@ -0,0 +1,20 @@
+const numberOne = 99;
+const numberTwo = 98;
+
+/**
+ * Number Operators
+ *
+ * sum: +
+ * sub: -
+ * mult: *
+ * div: /
+ * module: %
+ * power: **
+ */
+
+console.log(`${numberOne} + ${numberTwo}`, numberOne + numberTwo);
+console.log(`${numberOne} - ${numberTwo}`, numberOne - numberTwo);
+console.log(`${numberOne} * ${numberTwo}`, numberOne * numberTwo);
+console.log(`${numberOne} / ${numberTwo}`, numberOne / numberTwo);
+console.log('5 % 2', 5 % 2);
+console.log('5 ** 2', 5 ** 2);
---
+++ src/operators/string.js
@@ -0,0 +1,46 @@
+var userName = 'diegodisant';
+
+/**
+ * Concatenation
+ *
+ * + -> a += '0';
+ */
+
+console.log(userName + '0');
+
+userName = userName + '0';
+
+console.log(userName)
+
+userName += '1'; //overload
+
+console.log(userName);
+
+/**
+ * String operations
+ *
+ * concat: adds a string at the end
+ * trim: remove white spaces at start and end positions
+ * charAt: position at 0-index (0, 1, 2, 3, ...)
+ * replace: change one string by match
+ * includes: checks if one string is inside the string
+ */
+
+console.log(userName.concat('2'));
+
+userName = '      jonasilken      ';
+
+console.log(userName.trimEnd());
+console.log(userName.trimStart());
+
+userName = userName.trim();
+
+console.log(userName);
+
+console.log(userName.charAt(0));
+
+console.log(userName.replace('silk', 'salk'));
+
+console.log(userName.includes('silk'));
+
+console.log(userName.toUpperCase());
